a38580b1c7ce2aebc0285622d587e8398632f6c2
Marco De Gregorio <marco.degregorio@pagopa.it>
2023-02-21 10:22:48 +0100

    remove grace period

--- soft_bulk_delete/soft_bulk_delete.py
+++ soft_bulk_delete/soft_bulk_delete.py
@@ -1,27 +1,34 @@
-import os, requests, datetime
+import datetime
+import os
 from urllib.parse import urlencode
+
+import requests
+
 
 def chunks(lst, n):
     # Yield successive n-sized chunks from lst
     for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+        yield lst[i : i + n]
+
 
 ### ENTRY ###
 
-days_to_keep = 372 # 365+7 (one year + grace period)
-user = os.getenv('Z_USER')
-api_token = os.getenv('Z_API_TOKEN')
+days_to_keep = 365  # 1 year
+user = os.getenv("Z_USER")
+api_token = os.getenv("Z_API_TOKEN")
 
 # Build a query to find tickets CLOSED more than 1 year ago
 
 date_limit = datetime.datetime.now() - datetime.timedelta(days=days_to_keep)
 date_limit_s = f"{date_limit:%Y-%m-%d}"
 search_params = {
-    'query': 'type:ticket status:closed updated<' + date_limit_s,
-    'sort_by': 'updated_at',
-    'sort_order': 'asc'
+    "query": "type:ticket status:closed updated<" + date_limit_s,
+    "sort_by": "updated_at",
+    "sort_order": "asc",
 }
-search_url = 'https://pagopa.zendesk.com/api/v2/search.json?' + urlencode(search_params)
+search_url = "https://pagopa.zendesk.com/api/v2/search.json?" + urlencode(
+    search_params
+)
 
 ticket_ids = []
 while search_url:
@@ -29,25 +36,36 @@ while search_url:
     if response.status_code == 422:
         break
     elif response.status_code != 200:
-        print('Status:', response.status_code, 'Problem with the request. Exiting.')
+        print(
+            "Status:",
+            response.status_code,
+            "Problem with the request. Exiting.",
+        )
         exit(1)
     data = response.json()
-    for result in data['results']:
-        ticket_ids.append(result['id'])
-    search_url = data['next_page']
+    for result in data["results"]:
+        ticket_ids.append(result["id"])
+    search_url = data["next_page"]
 
-print('Found ' + str(len(ticket_ids)) + ' tickets to be deleted (closed BEFORE ' + date_limit_s + ').')
+print(
+    "Found "
+    + str(len(ticket_ids))
+    + " tickets to be deleted (closed BEFORE "
+    + date_limit_s
+    + ")."
+)
 
 for tickets in list(chunks(ticket_ids, 100)):
     tickets_str = [str(item) for item in tickets]
-    current_chunk = ','.join(tickets_str)
-    soft_destroy_params = {
-        'ids': current_chunk
-    }
-    soft_destroy_url = 'https://pagopa.zendesk.com/api/v2/tickets/destroy_many.json?' + urlencode(soft_destroy_params)
+    current_chunk = ",".join(tickets_str)
+    soft_destroy_params = {"ids": current_chunk}
+    soft_destroy_url = (
+        "https://pagopa.zendesk.com/api/v2/tickets/destroy_many.json?"
+        + urlencode(soft_destroy_params)
+    )
     response = requests.delete(soft_destroy_url, auth=(user, api_token))
     data = response.json()
-    print('\nJob issued: soft-deleting tickets # ' + current_chunk)
-    print(data['job_status']['url'])
+    print("\nJob issued: soft-deleting tickets # " + current_chunk)
+    print(data["job_status"]["url"])
 
-print('<soft_bulk_delete.py> - END')
+print("<soft_bulk_delete.py> - END")
