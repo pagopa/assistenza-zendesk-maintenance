e0ca37cc6c5cfdd82e51e4de8cab18488819f479
Marco De Gregorio <marco.degregorio@pagopa.it>
2024-10-16 15:27:28 +0200

    TT_1729085248

--- sso/app.py
+++ sso/app.py
@@ -5,14 +5,18 @@ from flask import Flask, redirect, request, send_from_directory
 app = Flask(__name__)
 BRAND_ID_PARTNER = "360004986777"
 BRAND_ID_SELFCARE = "9379749838865"
+BRAND_ID_SEND = "19650024163345"
 
 
 @app.route("/eplogin")
 def redirector():
     brand_id = request.args.get("brand_id")
     if brand_id == BRAND_ID_PARTNER:
-        # Portale Partner (skip SSO -> ZD native auth.)
-        return redirect("https://partner.assistenza.pagopa.it/access/normal")
+        return redirect("https://www.pagopa.gov.it")
+    elif brand_id == BRAND_ID_SEND:
+        return redirect("https://notifichedigitali.pagopa.it")
+    elif brand_id == BRAND_ID_SELFCARE:
+        return redirect("https://selfcare.pagopa.it")
     else:
         print("SSO LOGIN: No brand specified or not supported")
         return redirect("https://www.pagopa.it/it/societa/di-cosa-ci-occupiamo")
@@ -25,8 +29,11 @@ def sso_logout():
     message = request.args.get("message")
     print(f"SSO LOGOUT: {kind} - {message}")
     if brand_id == BRAND_ID_PARTNER:
-        # Portale Partner (skip SSO -> ZD native auth.)
-        return redirect("https://partner.assistenza.pagopa.it/access/normal")
+        return redirect("https://www.pagopa.gov.it/")
+    elif brand_id == BRAND_ID_SEND:
+        return redirect("https://notifichedigitali.pagopa.it")
+    elif brand_id == BRAND_ID_SELFCARE:
+        return redirect("https://selfcare.pagopa.it")
     else:
         print("SSO LOGOUT: No brand specified or not supported")
         return redirect("https://www.pagopa.it")
