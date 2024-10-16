0909b3f72ffcb9b199242b093dbd452ce3828e12
Marco De Gregorio <marco.degregorio@pagopa.it>
2024-10-16 15:54:29 +0200

    TT_1729086869

--- sso/app.py
+++ sso/app.py
@@ -6,17 +6,22 @@ app = Flask(__name__)
 BRAND_ID_PARTNER = "360004986777"
 BRAND_ID_SELFCARE = "9379749838865"
 BRAND_ID_SEND = "19650024163345"
+BRAND_ID_PAGOPA = "1900000625833"
+BRAND_ID_ARC = "27216507367697"
+BRAND_ID_APPIO = "360004986757"
 
 
 @app.route("/eplogin")
 def redirector():
     brand_id = request.args.get("brand_id")
-    if brand_id == BRAND_ID_PARTNER:
+    if brand_id == BRAND_ID_PARTNER or brand_id == BRAND_ID_PAGOPA:
         return redirect("https://www.pagopa.gov.it")
     elif brand_id == BRAND_ID_SEND:
         return redirect("https://notifichedigitali.pagopa.it")
     elif brand_id == BRAND_ID_SELFCARE:
         return redirect("https://selfcare.pagopa.it")
+    elif brand_id == BRAND_ID_APPIO:
+        return redirect("https://www.pagopa.it/it/prodotti-e-servizi/app-io")
     else:
         print("SSO LOGIN: No brand specified or not supported")
         return redirect("https://www.pagopa.it/it/societa/di-cosa-ci-occupiamo")
@@ -28,15 +33,17 @@ def sso_logout():
     kind = request.args.get("kind")
     message = request.args.get("message")
     print(f"SSO LOGOUT: {kind} - {message}")
-    if brand_id == BRAND_ID_PARTNER:
-        return redirect("https://www.pagopa.gov.it/")
+    if brand_id == BRAND_ID_PARTNER or brand_id == BRAND_ID_PAGOPA:
+        return redirect("https://www.pagopa.gov.it")
     elif brand_id == BRAND_ID_SEND:
         return redirect("https://notifichedigitali.pagopa.it")
     elif brand_id == BRAND_ID_SELFCARE:
         return redirect("https://selfcare.pagopa.it")
+    elif brand_id == BRAND_ID_APPIO:
+        return redirect("https://www.pagopa.it/it/prodotti-e-servizi/app-io")
     else:
         print("SSO LOGOUT: No brand specified or not supported")
-        return redirect("https://www.pagopa.it")
+        return redirect("https://www.pagopa.it/it/societa/di-cosa-ci-occupiamo")
 
 
 @app.route("/favicon.ico")
